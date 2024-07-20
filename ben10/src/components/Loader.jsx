5a62a4c508b6c0f03fa8a3e79363aacb8c3ed181
raju0005 <anderajesh15@gmail.com>
2024-07-20 22:45:37 +0530

    some css

--- ben10/src/components/Loader.jsx
+++ ben10/src/components/Loader.jsx
@@ -4,7 +4,9 @@ import './Loader.css'
 
 const Loader = () => {
   return (
-    <div className='loader-container'><img className='loader md:h-[200px] md:w-[200px] w-[100px] h-[200px] top-[25%] md:left-[46%] left-[40%]' src={loader} alt="" /></div>
+    <div className="w-full h-[70vh]  flex justify-center items-center ">
+      <img className="loader md:h-[200px] md:w-[200px] w-[100px] h-[100px]" src={loader} alt="Loader" />
+    </div>
   )
 }
 
