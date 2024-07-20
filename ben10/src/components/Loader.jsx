499682afd346ee91016ad96ce84ae2b0f7e498b3
raju0005 <anderajesh15@gmail.com>
2024-07-20 22:31:31 +0530

    Loader

--- ben10/src/components/Loader.jsx
+++ ben10/src/components/Loader.jsx
@@ -4,7 +4,7 @@ import './Loader.css'
 
 const Loader = () => {
   return (
-    <div className='loader-container'><img className='loader md:h-[200px] md:w-[200px] w-[100px] h-[200px]' src={loader} alt="" /></div>
+    <div className='loader-container'><img className='loader md:h-[200px] md:w-[200px] w-[100px] h-[200px] top-[25%] md:left-[46%] left-[40%]' src={loader} alt="" /></div>
   )
 }
 
