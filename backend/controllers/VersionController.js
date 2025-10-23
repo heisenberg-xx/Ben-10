54f67d1ed08c6a0df7e81994e689b8a7b3af508b
raju0005 <anderajesh15@gmail.com>
2025-10-24 01:16:57 +0530

    fix : slected fields to send as a res

--- backend/controllers/VersionController.js
+++ backend/controllers/VersionController.js
@@ -1,49 +1,55 @@
-import asyncHandler from "../middlewares/asynHandler.js"
-import asynHandler from "../middlewares/asynHandler.js"
-import Version from "../models/VersionModel.js"
-
+import asyncHandler from "../middlewares/asynHandler.js";
+import asynHandler from "../middlewares/asynHandler.js";
+import Version from "../models/VersionModel.js";
 
 const createVersions = asynHandler(async (req, res) => {
-    try {
-        const { name, description, logo, omnitrixLogo } = req.body
+  try {
+    const { name, description, logo, omnitrixLogo } = req.body;
 
-        const version = await new Version({ name, description, logo, omnitrixLogo }).save()
-        res.json(version)
-
-    } catch (error) {
-        console.log(error)
-        res.status(400).json(error.message);
-    }
-})
-
+    const version = await new Version({
+      name,
+      description,
+      logo,
+      omnitrixLogo,
+    }).save();
+    res.json(version);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+});
 
 const fetchAllVersions = asynHandler(async (req, res) => {
-    try {
-        const versions = await Version.find({})
-        res.status(200).json(versions)
-    } catch (error) {
-        console.log(error)
-        res.status(400).json(error.message);
-    }
-})
+  try {
+    const versions = await Version.find({}).select("_id name logo header");
+    res.status(200).json(versions);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+});
 
 const readVersion = asyncHandler(async (req, res) => {
-    try {
-        const version = await Version.findOne({
-            $or: [
-                { _id: req.params.versionId },
-                { name: req.params.versionName }
-            ]
-        })
-        res.json(version)
-    } catch (error) {
-        console.log(error)
-        res.status(400).json(error.message);
-    }
-})
+  try {
+    const version = await Version.findOne({
+      $or: [{ _id: req.params.versionId }, { name: req.params.versionName }],
+    });
+    res.json(version);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+});
+const readVersionById = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const version = await Version.findById(id);
 
+    res.json(version);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error.message);
+  }
+});
 
-
-
-
-export { fetchAllVersions, createVersions, readVersion }
+export { fetchAllVersions, createVersions, readVersion, readVersionById };
--- backend/routes/VersionRoutes.js
+++ backend/routes/VersionRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createVersions, fetchAllVersions, readVersion,  } from "../controllers/VersionController.js"
+import { createVersions, fetchAllVersions, readVersion, readVersionById,  } from "../controllers/VersionController.js"
 
 const router = express.Router()
 
 router.route("/").get(fetchAllVersions).post(createVersions)
 router.route("/:versionName").get(readVersion)
+router.route("/:id").get(readVersionById)
 
 
 
